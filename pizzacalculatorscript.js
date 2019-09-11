aec7f83d08154d51f5d46fc34b4fde632cd9f145
John Spruijt <99058314@mydavinci.nl>
2019-09-11 20:29:11 +0200

    new if statement for pizza value

--- pizzacalculatorscript.js
+++ pizzacalculatorscript.js
@@ -15,11 +15,14 @@ document.write("<br><br><br>")
 
 
 if (numberSmall <0) {                                                     //reactie van kleine pizza's
-    document.write("<font size='5' face='arial'><center>An unexpected error has occured, please try again")
+    document.write("<font size='5' face='arial'><center><font color='red'>An unexpected error has occured, please try again</font>")
     error = true
 }                                                                         //lettertype van hele pagina veranderd
-else if (numberSmall >0) {
+else if (numberSmall >1) {
     document.write("<font size='5' face='arial'><center>You chose "+numberSmall+" small pizza's     "+"    €"+numberSmall*11.99)
+}
+else if (numberSmall == 1){
+    document.write("<font size='5' face='arial'><center>You chose "+numberSmall+" small pizza     "+"    €"+numberSmall*11.99)
 }
 else if (numberSmall == 0) {
     document.write("<font size='5' face='arial'><center>You chose no small pizza's.     ")
@@ -33,16 +36,19 @@ document.write("<br><br>")                                                //rege
 
 
 if (numberMed <0) {                                                       //reactie van medium pizza's
-    document.write("An unexpected error has occured, please try again")   //minder dan 0 pizza's
+    document.write("<font color='red'>An unexpected error has occured, please try again</font>")   
     error = true
 }
-else if (numberMed >0) {                                                  //meer dan 0 pizza's
+else if (numberMed >1) {                                                  
     document.write("You chose "+numberMed+" medium pizza's"+"    €"+numberMed*12.99)
 }
-else if (numberMed == 0) {                                                //precies 0 pizza's
+else if (numberMed == 1) {
+    document.write("You chose "+numberMed+" medium pizza"+"    €"+numberMed*12.99)
+}
+else if (numberMed == 0) {                                                
     document.write("You chose no medium pizza's.")
 }
-else {                                                                    //iets anders dan cijfers
+else {                                                                    
     document.write("Only numbers are allowed.")
 }
 
@@ -51,11 +57,14 @@ document.write("<br><br>")                                                //rege
 
 
 if (numberLarge <0) {                                                     //reactie van Grote pizza's
-    document.write("An unexpected error has occured, please try again")
+    document.write("<font color='red'>An unexpected error has occured, please try again</font>")
     error = true
 }
-else if (numberLarge >0) {
+else if (numberLarge >1) {
     document.write("You chose "+numberLarge+" Large pizza's"+"    €"+numberLarge*13.99)
+}
+else if (numberLarge == 1){
+    document.write("You chose "+numberLarge+" Large pizza"+"    €"+numberLarge*13.99)
 }
 else if (numberLarge == 0){
     document.write("You chose no large pizza's.")
@@ -69,11 +78,11 @@ document.write("<br><br><br><br><br>")                                    //rege
 
 
 if (priceTotal <0) {                                                      //totale prijs weergeving
-    document.write("An unexpected error occured, please try again")
+    document.write("<font color='red'><b>An unexpected error has occured, please try again</b></font>")
 }
 else if (priceTotal >=0) {                                                //last error check
         if (error == true){
-            document.write("An unexpected error occured, please try again")
+            document.write("<font color='red'><b>An unexpected error has occured, please try again</b></font>")
         }
         else {
             document.write("<h3>Total : </h3>"+'<font size="9">€ '+priceTotal+'</font>')   
@@ -81,7 +90,7 @@ else if (priceTotal >=0) {                                                //last
      
 }
 else {
-    document.write("An unexpected error occured, please try again")
+    document.write("<font color='red'><b>An unexpected error has occured, please try again</b></font>")
     error = true
 }
     
